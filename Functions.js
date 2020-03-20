cad350b0f2d38703316b27c0416853189e133e1e
wandasscarpato <wandasscarpato@gmail.com>
2020-03-19 21:09:35 -0300

    Mi primer subida

---
+++ Functions.js
@@ -0,0 +1,17 @@
+function sumar(pepeA,pepeB){//es lo mismo que opA y opB
+    return pepeA+pepeB;
+}
+function restar(pepeA,pepeB){//es lo mismo que opA y opB
+    return pepeA-pepeB;
+}
+function multiplicar(pepeA,pepeB){//es lo mismo que opA y opB
+    return pepeA*pepeB;
+}
+function dividir(pepeA,pepeB){//es lo mismo que opA y opB
+    return pepeA/pepeB;
+}
+
+module.exports= [sumar,
+    "restar":restar,
+    "multiplicar":multiplicar,
+    "dividir":dividir];
