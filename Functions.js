40b8f44fbba587b3ea37c642e082bef1e6930208
wandasscarpato <62209510+wandasscarpato@users.noreply.github.com>
2020-03-19 21:47:56 -0300

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,11 +1,11 @@
-function sumar(pepeA,pepeB){//es lo mismo que opA y opB
+function sumar(pepeA,pepeB) {//es lo mismo que opA y opB
     return pepeA+pepeB;
 }
 function restar(pepeA,pepeB){//es lo mismo que opA y opB
     return pepeA-pepeB;
 }
 function multiplicar(pepeA,pepeB){//es lo mismo que opA y opB
-    return pepeA*pepeB;
+    return pepeA*pepeB; 
 }
 function dividir(pepeA,pepeB){//es lo mismo que opA y opB
     return pepeA/pepeB;
